c972abbb11a4cc0b9d25049f04df0d68eb8ca46a
Moaefqwf <47673821+Moaefqwf@users.noreply.github.com>
2019-05-22 08:45:56 -0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream ");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("580395697124409346")
+setInterval(function() {
+channel.send(`,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , ,tellmewhyimwitingfor , 
+`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
